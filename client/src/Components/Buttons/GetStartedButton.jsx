c2946fb9de161022a53c7b1bae50249baacc0439
Upendra-mu <upendrag2815@gmail.com>
2024-06-03 23:45:09 +0530

    Welcome Page UI

---
+++ client/src/Components/Buttons/GetStartedButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./GetStartedButton.css";
+const GetStartedButton = ({ label }) => {
+  return <button className="get-started-btn">{label}</button>;
+};
+
+export default GetStartedButton;
--- client/src/Screens/WelcomePage/WelcomePage.jsx
+++ client/src/Screens/WelcomePage/WelcomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import "./WelcomePage.css";
-
+import GetStartedButton from "../../Components/Buttons/GetStartedButton";
 const WelcomePage = () => {
   return (
     <Grid className="welcome-page-main">
@@ -10,8 +10,23 @@ const WelcomePage = () => {
           AU ChatBox
         </Grid>
 
-        <Grid item className="about" alignItems={"center"}>
+        <Grid item className="items" alignItems={"center"}>
           About
+        </Grid>
+        <Grid item className="items" alignItems={"center"}>
+          Features
+        </Grid>
+        <Grid item className="items" alignItems={"center"}>
+          Privacy
+        </Grid>
+        <Grid item className="items" alignItems={"center"}>
+          Help Center
+        </Grid>
+        <Grid item className="login" alignItems={"center"}>
+          Log In
+        </Grid>
+        <Grid item className="get-start" alignItems={"center"}>
+          <GetStartedButton label={"Get Started"} />
         </Grid>
       </Grid>
     </Grid>
